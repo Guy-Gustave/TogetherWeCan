c208d02a3bcfa0581bdfa04c1d9ea3d72bcfe870
RNtaate <royhyde14@gmail.com>
2021-12-10 12:26:43 +0300

    Add validations to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :first_name, presence: true, length: {minimum: 3}
+  validates :last_name, presence: true, length: {minimum: 3}
+  validates :telephone_number, presence: true, length: {minimum: 6}
+  validates :account_number, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :bank_name, presence: true
+  validates :country, presence: true
 end
