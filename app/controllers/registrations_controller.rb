e68fe5126a1ee6227dcd0618b777458d331883c4
RNtaate <royhyde14@gmail.com>
2021-12-23 22:50:19 +0300

    Update create function in registrations controller
    Uncomment wanted code of registration_params function

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,19 +1,10 @@
 class RegistrationsController < ApplicationController
-
+  
   def create
     if (params['user']['contribution'] && params['user']['membership_fee'] && params['user']['rules'])
-      user = User.new(
-        first_name: params['user']['first_name'],
-        last_name: params['user']['last_name'],
-        telephone_number: params['user']['telephone_number'],
-        account_number: params['user']['account_number'],
-        bank_name: params['user']['bank_name'],
-        country: params['user']['country'],
-        email: params['user']['email'],
-        password: params['user']['password'],
-        password_confirmation: params['user']['password_confirmation'],
-      )
-  
+
+      user = User.new(registration_params);
+
       if user.save
         session[:user_id] = user.id
         render json: {
@@ -30,7 +21,7 @@ class RegistrationsController < ApplicationController
       end
     else
       render json: {
-        errors: ["You must agree the terms and conditions, if not go away!"]
+        errors: {"Confirmation error:": ["You must agree to all the terms and conditions, if not go away!"]}
       }, status: 422
     end
     
@@ -38,9 +29,9 @@ class RegistrationsController < ApplicationController
 
   private
 
-  # def registration_params
-  #   params.require(:user).permit(:first_name, :last_name,:telephone_number, :account_number, :email, :password, :password_confirmation, :bank_name, :country, :identification_number, :contribution, :membership_fee, :rules)
-  # end
+  def registration_params
+    params.require(:user).permit(:first_name, :last_name,:telephone_number, :account_number, :email, :password, :password_confirmation, :bank_name, :country, :identification_number)
+  end
 
   # def confirmation_params 
   #   params.require(:user).permit(:)
