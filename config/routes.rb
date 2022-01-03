6b6c7b7520b3b12888ab741e8a489bd52f2dfada
RNtaate <royhyde14@gmail.com>
2022-01-03 23:33:28 +0300

    Add route for capitals controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :sessions, only: [:create]
   resources :registrations, only: [:create]
+  resources :capitals, only: [:index]
 
   delete :logout, to: "sessions#logout"
   get :logged_in, to: "sessions#logged_in"
