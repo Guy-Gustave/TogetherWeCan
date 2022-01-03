92a3ef5e14ef97c7f8989f8e0c6f745d26ae91d5
Guy <guymoustapha@gmail.com>
2022-01-03 11:58:26 +0200

    add email regex validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
   validates :last_name, presence: true, length: {minimum: 3}
   validates :telephone_number, presence: true, length: {minimum: 6}
   validates :account_number, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on:    :create }, uniqueness: {case_sensitive: false}, presence: true
+  # validates :email, presence: true, uniqueness: true
   validates :bank_name, presence: true
   validates :country, presence: true
 end
