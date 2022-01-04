5b1c5ef06c48e582d81be6f21015e65ee23ccb3b
RNtaate <royhyde14@gmail.com>
2022-01-05 01:25:20 +0300

    Create set_new_capital_name class method

--- app/models/capital.rb
+++ app/models/capital.rb
@@ -6,4 +6,18 @@ class Capital < ApplicationRecord
   validates :user, presence: true
   validates :capital_name, presence: true
 
+  def self.set_new_capital_name(current_user)
+    last_capital = Capital.where(user_id: current_user.id).last
+    new_capital_name = ""
+    if last_capital
+      new_capital_name = last_capital.capital_name
+      new_capital_name.delete_prefix!("capital")
+      new_capital_name = "capital" + (new_capital_name.to_i + 1).to_s;
+    else
+      new_capital_name = "capital1"
+    end
+
+    new_capital_name
+  end
+
 end
