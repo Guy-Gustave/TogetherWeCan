ffce477cc1d7efca50f88f9d1bea6de7e18f0640
Guy <guymoustapha@gmail.com>
2022-01-27 01:56:41 +0200

    Update create action,
    create index action in GiftsController

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -2,9 +2,17 @@ class GiftsController < ApplicationController
 
   include GiftsHelper
 
+  def index
+    gifts = Gift.where(capital_id: 2)
+    render json: {
+      gifts: gifts
+    }
+  end
+
   def create(current_user, capital)
-    @gift = Gift.new(user_id: current_user.id, capital_id: capital.id, amount: GIFT_AMOUNT)
+    @gift = Gift.new(user_id: current_user.id, capital_id: capital.id, amount: GIFT_AMOUNT, purchase_id: capital.purchase_id)
 
     @gift.save
+    @gift
   end
 end
