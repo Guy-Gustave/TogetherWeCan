284c8d5ca426aea9f2623c649d122dabbbe52055
RNtaate <royhyde14@gmail.com>
2022-01-28 00:50:24 +0300

    Add savings_amount, next_capitals columns to purchases
    Run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_24_211632) do
+ActiveRecord::Schema.define(version: 2022_01_27_214639) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,8 @@ ActiveRecord::Schema.define(version: 2022_01_24_211632) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.decimal "savings_amount"
+    t.integer "next_capitals"
     t.index ["user_id"], name: "index_purchases_on_user_id"
   end
 
