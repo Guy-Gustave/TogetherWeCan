f3bf442124172108f6ad5f5e5a0b5e26f9014e2b
RNtaate <royhyde14@gmail.com>
2022-01-28 02:00:53 +0300

    Create update_saving_in_purchases function
    Call it in create_payment function

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,6 +1,7 @@
 class TransactionsController < ApplicationController
 
   include CurrentUserConcern
+  include GiftsHelper
 
   def index
     @transactions = Transaction.all
@@ -60,6 +61,7 @@ class TransactionsController < ApplicationController
     end
     capital.save
 
+    update_saving_in_purchases(capital)
   end
 
   private
@@ -67,4 +69,20 @@ class TransactionsController < ApplicationController
   def capital_params
     params.require(:transaction).permit(:shares_number)
   end
+
+  def update_saving_in_purchases(capital)
+    purchase = Purchase.where(id: capital.purchase_id)
+
+    purchase.savings_amount = purchase.savings_amount.to_i if purchase.savings_amount == nil
+
+    purchase.savings_amount += SAVING_AMOUNT
+
+    if purchase.savings_amount == CAPITAL_AMOUNT
+      purchase.next_capitals = purchase.next_capitals.to_i + 1
+      purchase.savings_amount = 0
+    end
+
+    purchase.save
+  end
+
 end
