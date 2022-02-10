abe20f4ad50cb4f9f427742fdeb490395eb5208f
RNtaate <royhyde14@gmail.com>
2022-02-10 14:30:37 +0300

    Create get_gift_amount, get_saving_amount in gift_helper

--- app/helpers/gifts_helper.rb
+++ app/helpers/gifts_helper.rb
@@ -4,4 +4,16 @@ module GiftsHelper
   GIFT_AMOUNT = (CAPITAL_AMOUNT/5.5).round
 
   SAVING_AMOUNT = (CAPITAL_AMOUNT) / 2
+
+  def get_gift_amount(capital)
+    gift_amount = (capital.amount / 5.5).round
+    gift_amount = (capital.amount / 4.05).round if capital.purchase.week_number >= 17
+
+    gift_amount
+  end
+
+  def get_saving_amount(capital)
+    return (capital.amount / 2)
+  end
+
 end
