dc2c76efd98175c026547ba65720be97378ec56a
RNtaate <royhyde14@gmail.com>
2022-02-23 11:45:01 +0300

    Create invitations model
    Run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_17_215316) do
+ActiveRecord::Schema.define(version: 2022_02_22_145522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,13 @@ ActiveRecord::Schema.define(version: 2022_02_17_215316) do
     t.index ["capital_id"], name: "index_gifts_on_capital_id"
     t.index ["purchase_id"], name: "index_gifts_on_purchase_id"
     t.index ["user_id"], name: "index_gifts_on_user_id"
+  end
+
+  create_table "invitations", force: :cascade do |t|
+    t.integer "inviter_id"
+    t.integer "invitee_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "ishami_account_balances", force: :cascade do |t|
