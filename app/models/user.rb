6fc787f21fd3e0a7daf54deeb3f8907ef159313e
RNtaate <royhyde14@gmail.com>
2022-02-23 11:45:22 +0300

    Make associations between User, and invitations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,12 @@ class User < ApplicationRecord
   has_many :purchases, dependent: :destroy
   has_many :savings
 
+  has_many :invited_users, foreign_key: :inviter_id, class_name: 'Invitation', dependent: :destroy
+  has_many :invitees, through: :invited_users, dependent: :destroy
+
+  has_one :inviter_user, foreign_key: :invitee_id, class_name: 'Invitation', dependent: :destroy
+  has_one :inviter, through: :inviter_user, dependent: :destroy
+
   validates :first_name, presence: true, length: { minimum: 3 }
   validates :last_name, presence: true, length: { minimum: 3 }
   validates :telephone_number, presence: true, length: { minimum: 6 }
