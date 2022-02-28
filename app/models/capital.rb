a10a0188e002c6cf23d7621e9f47a4bca88c0c22
Guy-Gustave <guymoustapha@gmail.com>
2022-02-28 02:08:42 +0200

    Update set_new_capital_name function

--- app/models/capital.rb
+++ app/models/capital.rb
@@ -1,11 +1,13 @@
 class Capital < ApplicationRecord
+  include ApplicationHelper
+
   belongs_to :user
   belongs_to :purchase
   has_many :gifts, dependent: :destroy
   has_many :transactions, dependent: :destroy
   has_many :savings
 
-  CAPITAL_STATUS = ["original", "recreated"]
+  CAPITAL_STATUS = ['original', 'recreated']
 
   validates :amount, presence: true
   validates :user, presence: true
@@ -20,12 +22,9 @@ class Capital < ApplicationRecord
     end
   end
 
-
-
-
   def self.set_new_capital_name(purchase)
-    last_capital = Capital.where(user_id: purchase.user_id, purchase_id: purchase.id ).last
-    new_capital_name = ""
+    last_capital = purchase.week_number >= PHASE_2_WEEK ? Capital.where(user_id: purchase.user_id, purchase_id: purchase.id, phase_status: 'phase_2').last : Capital.where(user_id: purchase.user_id, purchase_id: purchase.id ).last
+    new_capital_name = ''
     if last_capital
       new_capital_name = last_capital.capital_name
       new_capital_name.delete_prefix!("capital-")
