6e6b2357034da63e83f48040a13b776a7abd0598
Guy-Gustave <guymoustapha@gmail.com>
2022-03-03 12:59:03 +0200

    Update render informations in purchase controller index action

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,13 +1,25 @@
 class PurchasesController < ApplicationController
+  include CurrentUserConcern
+  include CapitalsHelper
 
+  before_action :confirm_current_user, only: [:index]
 
-  def index 
-    @purchases = Purchase.all
+  def index
+    @purchases = @current_user.purchases.includes(:capitals)
+    total_capital_1_amount = 0
+    capitals = []
+    @purchases.each { |purchase| capitals += purchase.capitals }
+    # Use constant "CAPITAL_AMOUNT" below to maintain the capital amount returned to the front end even when the purchase week number is beyond week 33 where the actual capital amount changes. 
+    capitals.each { |cap| total_capital_1_amount += CAPITAL_AMOUNT if cap.capital_name == 'capital-1' }
+
     render json: {
-      purchases: @purchases
+      purchases: @purchases,
+      user_capitals: capitals,
+      shares_total_amount: total_capital_1_amount,
+      original_capital_amount: CAPITAL_AMOUNT
     }
   end
-  
+
   def create(current_user)
     last_purchase = Purchase.where(user_id: current_user.id).last
     purchase_no = 1
